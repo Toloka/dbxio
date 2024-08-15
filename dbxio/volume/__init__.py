29a53d358c7b1acc1da037c8f68ea427b73654e5
Nikita Yurasov <49479190+NikitaYurasov@users.noreply.github.com>
2024-08-15 11:17:04 +0200

    feat: support for managed volumes operations (#11)

--- dbxio/volume/__init__.py
+++ dbxio/volume/__init__.py
@@ -1,7 +1,9 @@
 from dbxio.volume.volume_commands import (
     Volume,
+    VolumeType,
     create_volume,
     download_volume,
+    drop_volume,
     get_comment_on_volume,
     get_tags_on_volume,
     set_comment_on_volume,
@@ -13,6 +15,7 @@ from dbxio.volume.volume_commands import (
 
 __all__ = [
     'Volume',
+    'VolumeType',
     'create_volume',
     'download_volume',
     'write_volume',
@@ -22,4 +25,5 @@ __all__ = [
     'get_tags_on_volume',
     'set_tags_on_volume',
     'unset_tags_on_volume',
+    'drop_volume',
 ]

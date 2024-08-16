aa9ab6c8d37fba4dbcabf2608f875f6e05046281
Nikita Yurasov <n.yurasov@yahoo.com>
2024-08-16 14:56:59 +0200

    do not create prefix structure for volumes download

--- dbxio/blobs/download.py
+++ dbxio/blobs/download.py
@@ -12,9 +12,12 @@ def download_blob_tree(
     object_storage_client: 'ObjectStorageClient', local_path: Path, prefix_path: Optional[str] = None
 ):
     for blob in object_storage_client.list_blobs(prefix=prefix_path):
+        if blob.name == prefix_path:
+            continue
+        relative_blob_path = blob.name[len(prefix_path) + 1 :] if prefix_path else blob.name
         if blob.content_settings.content_type is None:
             # it's a directory, create it
-            Path(local_path / blob.name).mkdir(parents=True, exist_ok=True)
+            Path(local_path / relative_blob_path).mkdir(parents=True, exist_ok=True)
             continue
 
-        object_storage_client.download_blob_to_file(blob.name, local_path / blob.name)
+        object_storage_client.download_blob_to_file(blob.name, local_path / relative_blob_path)

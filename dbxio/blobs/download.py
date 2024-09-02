8abd6471bc2e5c7ac0b50fc54a4c666a30af78a3
Nikita Yurasov <n.yurasov@yahoo.com>
2024-09-02 18:02:35 +0200

    fix: download subtree without clashes

--- dbxio/blobs/download.py
+++ dbxio/blobs/download.py
@@ -9,13 +9,25 @@ if TYPE_CHECKING:
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(10))
 def download_blob_tree(
-    object_storage_client: 'ObjectStorageClient', local_path: Path, prefix_path: Optional[str] = None
+    object_storage_client: 'ObjectStorageClient',
+    local_path: Path,
+    prefix_path: Optional[str] = None,
 ):
     for blob in object_storage_client.list_blobs(prefix=prefix_path):
-        if blob.name == prefix_path:
+        is_dir = object_storage_client.is_directory(blob.name)
+        if blob.name == prefix_path and is_dir:
+            # prefix path is subdir, skip it on first iteration
             continue
+
         relative_blob_path = blob.name[len(prefix_path) + 1 :] if prefix_path else blob.name
-        if blob.content_settings.content_type is None:
+        if not relative_blob_path:
+            # if the prefix path is full path to one file
+            relative_blob_path = blob.name[len(object_storage_client.blobs_path) + 1 :]
+
+        if is_dir:
+            if not blob.name.startswith(f'{prefix_path}/'):
+                # we found a directory with the same prefix, but it's not from our subtree
+                continue
             # it's a directory, create it
             Path(local_path / relative_blob_path).mkdir(parents=True, exist_ok=True)
             continue
